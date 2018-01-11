2f650c806cefbf9178f65fc08c9e6ccd32dca4d4
Emil Lundberg <emil@emlun.se>
2018-01-11 09:23:43 +0100

    Extract function getLimits

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -7,18 +7,21 @@ import * as mandelbrot from 'fractals/mandelbrot';
 const W = 300;
 const H = 200;
 
+function getLimits({ center, scale, W, H }) {
+  const aspectRatio = H / W;
+  const w = scale;
+  const h = scale * aspectRatio;
+  const topLeft = center.add(new Complex(-w/2, h/2));
+  const btmRight = center.add(new Complex(w/2, -h/2));
+  return { topLeft, btmRight };
+}
 
 function renderPixels(imageData, center, scale) {
   console.log('renderPixels', center, scale);
 
   const W = imageData.width;
   const H = imageData.height;
-
-  const aspectRatio = H / W;
-  const w = scale;
-  const h = scale * aspectRatio;
-  const topLeft = center.add(new Complex(-w/2, h/2));
-  const btmRight = center.add(new Complex(w/2, -h/2));
+  const { topLeft, btmRight } = getLimits({ center, scale, W, H });
 
   for (let x = 0; x < W; x += 1) {
     for (let y = 0; y < H; y += 1) {
