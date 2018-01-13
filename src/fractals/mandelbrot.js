369254c0fb14061e11c9688ddebf113e348f00d4
Emil Lundberg <emil@emlun.se>
2018-01-13 17:33:56 +0100

    Expose iterationLimit parameter in mandelbrot.check

--- src/fractals/mandelbrot.js
+++ src/fractals/mandelbrot.js
@@ -4,16 +4,16 @@ function step(z, c) {
   return z.mul(z).add(c);
 }
 
-function iterate(c, z, i = 0, escapeAbs = 2, iterationLimit = 255) {
+function iterate(c, z, iterationLimit = 255, i = 0, escapeAbs = 2) {
   if (z.abs() >= escapeAbs) {
     return i;
   } else if (i >= iterationLimit) {
     return -1;
   } else {
-    return iterate(c, step(z, c), i + 1, escapeAbs, iterationLimit);
+    return iterate(c, step(z, c), iterationLimit, i + 1, escapeAbs);
   }
 }
 
-export function check(c) {
-  return iterate(c, new Complex(0, 0));
+export function check(c, iterationLimit) {
+  return iterate(c, new Complex(0, 0), iterationLimit);
 }
