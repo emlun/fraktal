6364a77e58761570d538de7914b3e9218cf80d08
Emil Lundberg <emil@emlun.se>
2018-01-13 23:25:06 +0100

    Use <GithubCorner> instead of <GithubRibbon>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Canvas from 'components/Canvas';
-import GithubRibbon from 'components/GithubRibbon';
+import GithubCorner from 'components/GithubCorner';
 
 export default class App extends React.Component {
 
   render() {
     return <div>
-      <GithubRibbon repo="emlun/fraktal"/>
+      <GithubCorner repo="emlun/fraktal" fillColor="#626262" />
       <Canvas/>
     </div>;
   }
