242dccc4b2b00c8e9fbcf77afdafedbbf1d47511
Emil Lundberg <emil@emlun.se>
2018-01-13 19:50:00 +0100

    Make gradient support multiple segments

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -44,9 +44,9 @@ function renderPixels(imageData, matrix, palette) {
       const iterations = matrix[x][y];
 
       if (iterations > 0) {
-        imageData.data[y * W * 4 + x * 4] = palette.getIn([0, iterations], 0);
-        imageData.data[y * W * 4 + x * 4 + 1] = palette.getIn([1, iterations], 0);
-        imageData.data[y * W * 4 + x * 4 + 2] = palette.getIn([2, iterations], 0);
+        imageData.data[y * W * 4 + x * 4] = palette.getIn([0, iterations], 255);
+        imageData.data[y * W * 4 + x * 4 + 1] = palette.getIn([1, iterations], 255);
+        imageData.data[y * W * 4 + x * 4 + 2] = palette.getIn([2, iterations], 255);
       } else {
         imageData.data[y * W * 4 + x * 4] = 0;
         imageData.data[y * W * 4 + x * 4 + 1] = 0;
@@ -59,6 +59,10 @@ function renderPixels(imageData, matrix, palette) {
   return imageData;
 }
 
+
+const defaultBottom = Immutable.fromJS({ value: 0, color: [0, 0, 0] });
+const defaultTop = Immutable.fromJS({ value: 255, color: [255, 255, 255] });
+
 export default class Canvas extends React.Component {
 
   constructor(props) {
@@ -70,10 +74,10 @@ export default class Canvas extends React.Component {
           height: 200,
           width: 300,
         }),
-        gradient: Immutable.fromJS({
-          bottom: [0, 0, 0],
-          top: [255, 255, 255],
-        }),
+        gradient: Immutable.fromJS([
+          defaultBottom,
+          defaultTop,
+        ]),
         matrix: [[]],
         scale: 2.5,
         status: undefined,
@@ -124,11 +128,21 @@ export default class Canvas extends React.Component {
     if (this.canvas) {
       console.log('About to render pixels...');
 
-      const palette = Immutable.Range(0, 3).map(c =>
-        Immutable.Range(0, 256).map(i =>
-          (this.get(['gradient', 'top', c]) - this.get(['gradient', 'bottom', c])) * (i / 255.0) + this.get(['gradient', 'bottom', c])
-        )
-      );
+      const bottom = this.get(['gradient', 0], defaultBottom).set('value', 0);
+      const gradient = Immutable.List([bottom]).concat(this.get(['gradient']));
+
+      const palette = Immutable.Range(0, 3).map(c => {
+        return Immutable.List([gradient.first().getIn(['color', c])]).concat(gradient.skip(1).flatMap((pivot, prevIndex) => {
+          const prev = gradient.get(prevIndex);
+          const start = prev.get('value');
+          const end = pivot.get('value');
+          const diff = end - start;
+
+          return Immutable.Range(1, diff + 1).map(segmentIndex =>
+            prev.getIn(['color', c]) + (segmentIndex * 1.0 / diff) * (pivot.getIn(['color', c]) - prev.getIn(['color', c]))
+          );
+        }));
+      });
 
       const ctx = this.canvas.getContext('2d');
 
@@ -136,7 +150,7 @@ export default class Canvas extends React.Component {
         const imageData = renderPixels(
           ctx.getImageData(0, 0, this.get(['dimensions', 'width']), this.get(['dimensions', 'height'])),
           this.get(['matrix']),
-          palette
+          Immutable.fromJS(palette.toJS())
         );
 
         ctx.putImageData(imageData, 0, 0);
@@ -155,7 +169,8 @@ export default class Canvas extends React.Component {
         this.get(['dimensions', 'width']),
         this.get(['dimensions', 'height']),
         this.get(['center']),
-        this.get(['scale'])
+        this.get(['scale']),
+        this.get(['gradient']).last().get('value')
       );
 
       console.log('Saving matrix', matrix);
@@ -206,16 +221,16 @@ export default class Canvas extends React.Component {
     this.update(['scale'], scale => scale * 2);
   }
 
-  componentWillUpdate(newProps, newState) {
+  componentDidUpdate(prevProps, prevState) {
     if (
-      newState.state.get('center') !== this.get(['center'])
-        || newState.state.get('scale') !== this.get(['scale'])
+      prevState.state.get('center') !== this.get(['center'])
+        || prevState.state.get('scale') !== this.get(['scale'])
     ) {
       this.computeMatrix();
     }
     if (
-      newState.state.get('matrix') !== this.get(['matrix'])
-        || newState.state.get('gradient') !== this.get(['gradient'])
+      prevState.state.get('matrix') !== this.get(['matrix'])
+        || prevState.state.get('gradient') !== this.get(['gradient'])
     ) {
       this.renderPixels();
     }
@@ -258,22 +273,51 @@ export default class Canvas extends React.Component {
         </p>
         <p> { this.get(['status']) } </p>
 
-        <p>
-          Gradient top:
-          <input type="color"
-            value={ '#' + this.get(['gradient', 'top']).map(d => sprintf('%02x', d)).join('') }
-            onChange={
-              ({ target: { value } }) =>
-                this.set(['gradient', 'top'],
-                  Immutable.List([
-                    parseInt(value.substring(1, 3), 16),
-                    parseInt(value.substring(3, 5), 16),
-                    parseInt(value.substring(5, 7), 16),
-                  ])
-                )
-            }
-          />
-        </p>
+        <div>
+          Gradient:
+          { this.get(['gradient']).map((pivot, index) =>
+            <div key={ index }>
+              <input type="number"
+                value={ pivot.get('value') }
+                onChange={
+                  ({ target: { value } }) =>
+                    this.set(['gradient', index, 'value'], Math.max(0, Math.min(parseInt(value), this.get(['gradient', index + 1, 'value'], Infinity))))
+                }
+              />
+              <input type="color"
+                value={ '#' + pivot.get('color').map(d => sprintf('%02x', d)).join('') }
+                onChange={
+                  ({ target: { value } }) =>
+                    this.set(['gradient', index, 'color'],
+                      Immutable.List([
+                        parseInt(value.substring(1, 3), 16),
+                        parseInt(value.substring(3, 5), 16),
+                        parseInt(value.substring(5, 7), 16),
+                      ])
+                    )
+                }
+              />
+              <button type="button"
+                onClick={ () => {
+                  const next = this.get(['gradient', index + 1]);
+                  if (next) {
+                    const middle = pivot
+                      .set('value', Math.round((pivot.get('value') + next.get('value')) / 2.0))
+                      .set('color', pivot.get('color').zipWith((c1, c2) => Math.round((c1 + c2) / 2.0), next.get('color')))
+                    ;
+
+                    this.update(['gradient'], gradient => gradient.insert(index + 1, middle));
+                  } else {
+                    this.update(['gradient'], gradient => gradient.insert(index, gradient.get(index)));
+                  }
+                } }
+              > + </button>
+              <button type="button"
+                onClick={ () => this.update(['gradient'], gradient => gradient.delete(index)) }
+              > - </button>
+            </div>
+          ) }
+        </div>
 
       </form>
 
