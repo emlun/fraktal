3d331bb3b06f5cd6baba5521e1542d62de853d37
Emil Lundberg <emil@emlun.se>
2018-01-13 21:45:36 +0100

    Add GitHub ribbon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Canvas from 'components/Canvas';
+import GithubRibbon from 'components/GithubRibbon';
 
 export default class App extends React.Component {
 
   render() {
     return <div>
+      <GithubRibbon repo="emlun/fraktal"/>
       <Canvas/>
     </div>;
   }
