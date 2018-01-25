8f867f57268b0d3f4fe2f718b68d07db18ba5d6f
Emil Lundberg <emil@emlun.se>
2018-01-25 20:37:15 +0100

    Add component <FloatInput>

---
+++ src/components/FloatInput.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'underscore';
+
+export default class FloatInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.value,
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  update(newValue) {
+    this.setState({ value: newValue });
+    const parsed = parseFloat(newValue);
+    if (!_.isNaN(parsed)) {
+      this.props.onChange(parsed);
+      console.log('onChange', parsed);
+    }
+  }
+
+  onChange({ target: { value } }) {
+    this.update(value);
+  }
+
+  render() {
+    return <input
+      onChange={ this.onChange }
+      placeholder={ this.props.placeholder }
+      type="text"
+      value={ this.state.value }
+    />;
+  }
+
+}
+
+FloatInput.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
