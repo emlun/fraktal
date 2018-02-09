faf8c00e4bd736e146f09d2c04112285f9ebe4a6
Emil Lundberg <emil@emlun.se>
2018-02-10 00:37:35 +0100

    Extract const aspectRatio in function computeNumberAt

--- src/reducers/viewpoint.js
+++ src/reducers/viewpoint.js
@@ -5,10 +5,11 @@ import ViewpointState from 'data/Viewpoint';
 
 
 function computeNumberAt({ center, dimensions, scale, x, y }) {
+  const aspectRatio = dimensions.get('height') / dimensions.get('width');
   return center.add(
     new Complex(
       ((x / dimensions.get('width')) - 0.5) * scale,
-      (0.5 - (y / dimensions.get('height'))) * scale * (dimensions.get('height') / dimensions.get('width'))
+      (0.5 - (y / dimensions.get('height'))) * scale * aspectRatio
     )
   );
 }
