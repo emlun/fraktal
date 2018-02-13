5887a619b787b21934eba382ad70c2961ed3bc25
Emil Lundberg <emil@emlun.se>
2018-02-13 21:05:55 +0100

    Disconnect <App> from Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
-import AppState from 'data/AppState';
 
 import Canvas from 'components/Canvas';
 import Controls from 'components/Controls';
@@ -21,69 +17,31 @@ function computeTreeRef() {
 }
 
 
-class App extends React.Component {
+export default function App() {
+  return <div styleName="wrapper">
+    <div styleName="main">
+      <GithubCorner
+        fillColor="#626262"
+        repo="emlun/fraktal"
+      />
+      <Canvas/>
+      <Controls/>
+    </div>
 
-  constructor(props) {
-    super(props);
-
-    this.update = this.update.bind(this);
-  }
-
-  update(pathOrUpdater, updater) {
-    if (updater) {
-      this.props.onSetState(this.props.state.updateIn(pathOrUpdater, updater));
-    } else {
-      this.props.onSetState(this.props.state.update(pathOrUpdater));
-    }
-  }
-
-  render() {
-    return <div styleName="wrapper">
-      <div styleName="main">
-        <GithubCorner
-          fillColor="#626262"
-          repo="emlun/fraktal"
-        />
-        <Canvas
-          state={ this.props.state }
-          update={ this.update }
-        />
-        <Controls/>
+    <footer styleName="footer">
+      <div>
+        <a href="https://emlun.se/">
+          { 'emlun.se' }
+        </a>
       </div>
-
-      <footer styleName="footer">
-        <div>
-          <a href="https://emlun.se/">
-            { 'emlun.se' }
-          </a>
-        </div>
-        <div styleName="middle">
-          { PROJECT_NAME }
-          { ' ' }
-          <a href={ `https://github.com/emlun/fraktal/tree/${computeTreeRef()}` }>
-            { VERSION }
-          </a>
-        </div>
-        <div/>
-      </footer>
-    </div>;
-  }
-
+      <div styleName="middle">
+        { PROJECT_NAME }
+        { ' ' }
+        <a href={ `https://github.com/emlun/fraktal/tree/${computeTreeRef()}` }>
+          { VERSION }
+        </a>
+      </div>
+      <div/>
+    </footer>
+  </div>;
 }
-App.propTypes = {
-  state: PropTypes.instanceOf(AppState).isRequired,
-  onSetState: PropTypes.func.isRequired,
-};
-
-const AppContainer = connect(
-  state => ({
-    state,
-  }),
-  {
-    onSetState: state => ({
-      type: 'SET_STATE',
-      state,
-    }),
-  }
-)(App);
-export default AppContainer;
