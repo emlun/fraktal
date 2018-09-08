9b9e8806f961f55ab6c4d510fffae2fda2761ebb
Emil Lundberg <emil@emlun.se>
2018-09-08 02:50:26 +0200

    Delete unused action

--- src/reducers/viewpoint.js
+++ src/reducers/viewpoint.js
@@ -1,19 +1,9 @@
 import * as actions from 'actions/viewpoint';
 import ViewpointState from 'data/Viewpoint';
-import { computeNumberAt } from 'util/view';
 
 
 export default function reducer(state = new ViewpointState(), action) {
   switch (action.type) {
-    case actions.CENTER_VIEW:
-      return state.set('center', computeNumberAt({
-        center: state.get('center'),
-        dimensions: state.get('dimensions'),
-        scale: state.get('scale'),
-        x: action.x,
-        y: action.y,
-      }));
-
     case actions.SET_CENTER:
       return state.set('center', action.center);
 
