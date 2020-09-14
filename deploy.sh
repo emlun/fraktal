d3f33d4a6dfd87494c77aaab6d6ff659c591de9b
Emil Lundberg <emil@emlun.se>
2020-09-15 01:38:26 +0200

    Use wasm-pack from npm installation

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ set -x
 
 cargo test
 cd www && npm run lint && cd -
-wasm-pack build
+./www/node_modules/.bin/wasm-pack build
 cd www
 npm run build
 rsync -avP --delete build/ emlun.se:/srv/http/public/fraktal/
