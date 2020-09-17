811a611b54205ba8a4c7f92ef9b2a191310674ab
Emil Lundberg <emil@emlun.se>
2020-09-17 05:06:54 +0200

    Instantiate Engine in <App> instead of module root

--- www/src/components/App.tsx
+++ www/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import Canvas from 'components/Canvas';
 import Controls from 'components/Controls';
@@ -8,9 +8,6 @@ import GithubCorner from 'components/GithubCorner';
 import { Engine } from 'fraktal-wasm';
 
 import styles from './App.module.module.css';
-
-
-const engine = Engine.new();
 
 
 function computeTreeRef() {
@@ -25,36 +22,42 @@ function computeTreeRef() {
 function App() {
   const [sidebarExpanded, setSidebarExpanded] = useState(true);
 
-  return <div className={ styles.wrapper }>
-    <GithubCorner
-      fillColor="#626262"
-      repo="emlun/fraktal"
-      visible={ sidebarExpanded }
-    />
-    <Canvas engine={ engine }/>
-    <Sidebar
-      expanded={ sidebarExpanded }
-      onToggle={ () => setSidebarExpanded(!sidebarExpanded) }
-      title="Settings"
-    >
-      <Controls engine={ engine } />
+  const engine = useMemo(Engine.new, [Engine]);
 
-      <footer className={ styles.footer }>
-        <div>
-          { PROJECT_NAME }
-          { ' ' }
-          <a href={ `https://github.com/emlun/fraktal/tree/${computeTreeRef()}` }>
-            { VERSION }
-          </a>
-        </div>
-        <div>
-          <a href="https://emlun.se/">
-            emlun.se
-          </a>
-        </div>
-      </footer>
-    </Sidebar>
-  </div>;
+  if (engine) {
+    return <div className={ styles.wrapper }>
+      <GithubCorner
+        fillColor="#626262"
+        repo="emlun/fraktal"
+        visible={ sidebarExpanded }
+      />
+      <Canvas engine={ engine }/>
+      <Sidebar
+        expanded={ sidebarExpanded }
+        onToggle={ () => setSidebarExpanded(!sidebarExpanded) }
+        title="Settings"
+      >
+        <Controls engine={ engine } />
+
+        <footer className={ styles.footer }>
+          <div>
+            { PROJECT_NAME }
+            { ' ' }
+            <a href={ `https://github.com/emlun/fraktal/tree/${computeTreeRef()}` }>
+              { VERSION }
+            </a>
+          </div>
+          <div>
+            <a href="https://emlun.se/">
+              emlun.se
+            </a>
+          </div>
+        </footer>
+      </Sidebar>
+    </div>;
+  } else {
+    return <>Loading...</>;
+  }
 }
 
 export default App;
