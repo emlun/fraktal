840695bf0203dc56e1ca944da13d5f72ccf00d50
Emil Lundberg <emil@emlun.se>
2023-07-01 15:37:32 +0200

    Fix wasm module resolution order

--- www/src/bootstrap.ts
+++ www/src/bootstrap.ts
@@ -1,5 +1,9 @@
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
-import('./index')
+
+// First make sure wasm is initialized correctly
+import('fraktal-wasm')
+  // Then launch the app
+  .then(() => import('./index'))
   .catch(e => console.error('Error importing `index`:', e));
