8036395abfd18d1f34872ed6c9e2f147b07acbe6
Emil Lundberg <emil@emlun.se>
2023-07-14 18:03:03 +0200

    Remove wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,6 @@ use crate::rect::RectRegion;
 use crate::utils::Pristine;
 use crate::utils::Ratchet;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 pub struct Color {
