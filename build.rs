e1058482ecdbb302b7996f86e53ea78e80f204ec
Emil Lundberg <emil@emlun.se>
2023-07-19 01:25:42 +0200

    Use git2 library instead of subprocess in build.rs

--- build.rs
+++ build.rs
@@ -1,20 +1,21 @@
+use git2::DescribeFormatOptions;
+use git2::DescribeOptions;
+use git2::Repository;
 use std::path::Path;
-use std::process::Command;
 
 fn main() {
     let out_dir = std::env::var_os("OUT_DIR").expect("Failed to determine output directory");
     let dest_path = Path::new(&out_dir).join("version.rs");
 
-    let version: Option<String> = Command::new("git")
-        .arg("describe")
-        .arg("--always")
-        .arg("--tags")
-        .arg("--match=v*")
-        .arg("--long")
-        .output()
-        .ok()
-        .map(|o| o.stdout)
-        .and_then(|o| String::from_utf8(o).ok())
+    let version: Option<String> = std::env::var_os("CARGO_MANIFEST_DIR")
+        .map(|dir| {
+            Repository::open(dir)?
+                .describe(DescribeOptions::new().describe_tags().pattern("v*"))?
+                .format(Some(
+                    DescribeFormatOptions::new().always_use_long_format(true),
+                ))
+        })
+        .and_then(Result::ok)
         .map(|v| v.trim().replacen('-', ".", 1));
 
     std::fs::write(
