ad7f4bd3f8e06281151728b09158ddf66d3d115d
Emil Lundberg <emil@emlun.se>
2023-07-21 11:32:45 +0200

    Use for instead of while loop in mandelbrot::check

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -2,16 +2,14 @@ use super::complex::Complex;
 
 pub fn check(c: Complex<f64>, iteration_limit: usize, escape_abs_squared: f64) -> usize {
     let mut z = c;
-    let mut i = 0;
 
-    while i < iteration_limit {
+    for i in 0..iteration_limit {
         let (abs, z2) = z.abs_squared_and_square();
         if abs >= escape_abs_squared {
             return i;
         }
 
         z = z2 + c;
-        i += 1;
     }
 
     iteration_limit
